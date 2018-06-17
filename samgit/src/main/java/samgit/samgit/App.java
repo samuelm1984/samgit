dfba4fe60ca62c70bd976c00fce237a8287b0871
user <user@user-PC>
2018-06-17 14:06:57 +0530

    initial samgit

---
+++ samgit/src/main/java/samgit/samgit/App.java
@@ -0,0 +1,13 @@
+package samgit.samgit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
